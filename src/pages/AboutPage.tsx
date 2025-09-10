3217e693940f4c0532be36030b34e0e1784bf4a6
Sunhith Reddy T <150027199+SunhithReddy647@users.noreply.github.com>
2025-09-10 10:28:01 +0530

    Updated AboutPage.tsx

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -1,12 +1,3 @@
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-FGE095VZMR"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-FGE095VZMR');
-</script>
 import React from 'react';
 import { Code2, Users, Target, Award, Lightbulb, Rocket, Heart, Star } from 'lucide-react';
 
@@ -208,3 +199,12 @@ export const AboutPage: React.FC = () => {
     </div>
   );
 };
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-FGE095VZMR"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-FGE095VZMR');
+</script>
